5778486bce885c4f6991d3b127e07c0f8d9a48d2
Mikaël Sévigny <sevignator@hotmail.com>
2024-09-30 07:08:02 -0400

    Add missing label

--- src/SelectInput.tsx
+++ src/SelectInput.tsx
@@ -1,19 +1,24 @@
 import React from 'react';
 
 export default function SelectInput({
+  labelText,
   options,
   name,
   ...props
 }: React.ComponentProps<'select'> & {
+  labelText: string;
   options: [value: string, text: string][];
 }) {
   const id = React.useId();
 
   return (
-    <select {...props} name={name} id={id}>
-      {options.map((option) => (
-        <option value={option[0]}>{option[1]}</option>
-      ))}
-    </select>
+    <>
+      <label htmlFor={id}>{labelText}</label>
+      <select {...props} name={name} id={id}>
+        {options.map((option) => (
+          <option value={option[0]}>{option[1]}</option>
+        ))}
+      </select>
+    </>
   );
 }
