cf8b6010241badfbb0641cce5a2fc2bf2dbfbb12
Mikaël Sévigny <sevignator@hotmail.com>
2024-09-30 06:45:43 -0400

    Add missing labelText prop

--- src/Checkbox.tsx
+++ src/Checkbox.tsx
@@ -2,20 +2,22 @@ import React from 'react';
 
 export default function CheckboxInput({
   name,
+  labelText,
   labelPosition = 'after',
   ...props
 }: React.ComponentProps<'input'> & {
+  labelText: string;
   labelPosition: 'before' | 'after';
 }) {
   const id = React.useId();
 
   return (
     <label htmlFor={id}>
-      {labelPosition === 'before' && labelPosition}
+      {labelPosition === 'before' ? labelText : ''}
 
       <input {...props} type='checkbox' name={name} id={id} />
 
-      {labelPosition === 'after' && labelPosition}
+      {labelPosition === 'after' ? labelText : ''}
     </label>
   );
 }
